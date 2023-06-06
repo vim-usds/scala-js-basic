a477193e6055517f8618e2787ea8c1c538a445a4
Vim USDS <vimal.k.shah@omb.eop.gov>
2023-06-05 20:37:57 -0700

    add DOM and try JS exception wrapper

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ scalaVersion := "3.2.2"
 
 // This is an application with a main method
 scalaJSUseMainModuleInitializer := true
+
+libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "2.2.0"
--- src/main/scala/tutorial/webapp/TutorialApp.scala.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala.scala
@@ -1,7 +1,35 @@
 package tutorial.webapp
 
+import org.scalajs.dom
+import org.scalajs.dom.document
+
+import scala.scalajs.js.annotation.JSExportTopLevel
+
+import scala.scalajs.js
+
 object TutorialApp {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    appendPar(document.body, "Hello World")
+  }
+
+  def appendPar(targetNode: dom.Node, text: String): Unit = {
+    val parNode = document.createElement("p")
+    parNode.textContent = text
+    targetNode.appendChild(parNode)
+  }
+
+  @JSExportTopLevel("addClickedMessage")
+  def addClickedMessage(): Unit = {
+    appendPar(document.body, "You clicked the button!")
+  }
+
+  @JSExportTopLevel("testError")
+  def testError():Unit = {
+    try{
+      require(1==0, "sorry 1 doesn't equal 0")
+    } catch {
+      case e: Throwable => throw js.JavaScriptException(e)
+    }
   }
 }
+
